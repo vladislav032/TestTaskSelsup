a0cde1ac3ab70a5c322bc221dcb587e271a60a0b
vlados <velikiy6789@gmail.com>
2025-04-01 23:31:07 +0300

    добовление новых файлов для тестового окружения

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
+
+export default config;
